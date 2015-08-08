831c8b5bb34b0287dc7b9e1cc2bfa036503d9521
Nishant Rodrigues <nishantjr@gmail.com>
2015-08-08 18:48:55 +0900

    Make `./jekyll release` command work.
    * Drop our old commit-build script.
    * Add git-commit-filetree from
      https://github.com/cjs-cynic-net/git-commit-filetree
    * Make our release plugin (_plugins/release.rb) call it.

--- _plugins/release.rb
+++ _plugins/release.rb
@@ -3,11 +3,20 @@ class ReleaseCommand < Jekyll::Command
         def init_with_program(prog)
             prog.command(:release) { |c|
                 c.syntax "release"
-                c.description 'Release current build to production web server.'
+                c.description 'Release current build to gh-pages branch.'
                 c.action { |args, options|
-                    puts("XXX Write me!")
+                    build_options = options.dup
+                    build_options['quiet'] = true
+                    Jekyll::Commands::Build.process(build_options)
+                    process(options)
                 }
             }
+        end
+
+        def process(options)
+            dest = configuration_from_options(options)['destination']
+            system('./_plugins/git-commit-filetree', 'gh-pages', dest) \
+                || fail('git-commit-filetree failed.')
         end
     end
 end
